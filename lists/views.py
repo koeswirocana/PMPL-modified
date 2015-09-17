15b353a5b70b540ceb553ea72913482aa56c1769
koeswirocana <koeswirocana@yahoo.co.id>
2015-09-17 11:16:56 +0700

    Selesai latihan dua dengan tambahan fitur komentar

--- lists/views.py
+++ lists/views.py
@@ -4,7 +4,15 @@ from lists.models import Item
 def home_page(request):
 	if request.method == 'POST':
 		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/')		
+		return redirect('/')
+
+	comments = 'yey, waktunya berlibur'
+
+	if Item.objects.count() > 0:
+		comments = 'sibuk tapi santai'
+
+	if Item.objects.count() >= 5:
+		comments = 'oh tidak'
 
 	items = Item.objects.all()
-	return render(request, 'home.html', {'items': items})
+	return render(request, 'home.html', {'items': items, 'comments': comments})
