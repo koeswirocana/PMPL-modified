8fd2d28c8315a0203e90e3cf9d3ff911fc71892e
koeswirocana <koeswirocana@yahoo.co.id>
2015-09-19 16:45:09 +0700

    new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -4,7 +4,7 @@ from lists.models import Item
 def home_page(request):
 	if request.method == 'POST':
 		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/')
+		return redirect('/lists/the-only-list-in-the-world/')
 
 	comments = 'yey, waktunya berlibur'
 
@@ -15,4 +15,8 @@ def home_page(request):
 		comments = 'oh tidak'
 
 	items = Item.objects.all()
-	return render(request, 'home.html', {'items': items, 'comments': comments})
+	return render(request, 'home.html', {'comments': comments})
+
+def view_list(request):
+	items = Item.objects.all()
+	return render(request, 'list.html', {'items': items})
